739f7f5c78b0705cffad5d376f9edd8609101553
bkpark71 <121483369+bkpark71@users.noreply.github.com>
2023-06-09 16:48:58 +0900

    수정

--- src/App3.jsx
+++ src/App3.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './routes/Home';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+//import { HashRouter } from 'react-router-dom';
 import './App.css';
 import About from "./routes/About";
 import Navigation from './components/Navigation';
@@ -11,12 +12,12 @@ class App3 extends React.Component{
   // /about 을 하면 home을 렌더링하고 그 끝에 about을 렌더링하는 현상을 막아줘야 한다.
   render() {
     return (
-        <HashRouter>
+      <BrowserRouter>
           <Navigation />
           <Route path="/" exact={true} component={Home} /> 
           <Route path="/about" component={About} /> 
           <Route path="/movie-detail" component={Detail} /> 
-        </HashRouter>
+      </BrowserRouter>
     )
   }
 }
